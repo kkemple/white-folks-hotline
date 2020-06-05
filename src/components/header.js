ade9089b30d14d35bf0387175bc69e089453263e
mvhoute <mvhoute@gmail.com>
2020-06-05 18:22:21 +0200

    Initial commit from gatsby: (https://github.com/gatsbyjs/gatsby-starter-default.git)

---
+++ src/components/header.js
@@ -0,0 +1,32 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      background: `black`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0, color: '#fff' }}>{siteTitle}</h1>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet</p>
+  </Layout>
+)
+
+export default IndexPage
